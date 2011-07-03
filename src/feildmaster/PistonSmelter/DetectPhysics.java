b305ad1712b0f6c99cb05f09bd5368c26517a22e
Feildmaster <admin@feildmaster.com>
2011-07-03 04:28:09 -0500

    Uploading PistonSmelter v1.5

---
+++ src/feildmaster/PistonSmelter/DetectPhysics.java
@@ -0,0 +1,70 @@
+package feildmaster.PistonSmelter;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockListener;
+import org.bukkit.event.block.BlockPhysicsEvent;
+import org.bukkit.inventory.ItemStack;
+
+class DetectPhysics extends BlockListener {
+
+    public DetectPhysics() {}
+
+    protected Boolean next2Piston(Block block) {
+    if (block.getFace(BlockFace.EAST).getType() == Material.PISTON_EXTENSION
+            || block.getFace(BlockFace.WEST).getType() == Material.PISTON_EXTENSION
+            || block.getFace(BlockFace.NORTH).getType() == Material.PISTON_EXTENSION
+            || block.getFace(BlockFace.SOUTH).getType() == Material.PISTON_EXTENSION)
+      return true;
+    
+    return false;
+  }
+
+    public void onBlockBreak(BlockBreakEvent event) {
+        Block block = event.getBlock();
+        if (block.getData() == (byte)15) {
+            if(block.getType() == Material.STONE) {
+                ItemStack stone = new ItemStack(Material.STONE, 1);
+                event.getPlayer().getWorld().dropItemNaturally(block.getLocation(), stone);
+                block.setType(Material.AIR);
+                event.setCancelled(true);
+            } else if (block.getType() == Material.IRON_BLOCK) {
+                ItemStack bi = new ItemStack(Material.IRON_INGOT, 1);
+                event.getPlayer().getWorld().dropItemNaturally(block.getLocation(), bi);
+                block.setType(Material.AIR);
+                event.setCancelled(true);
+            } else if (block.getType() == Material.GOLD_BLOCK) {
+                ItemStack bi = new ItemStack(Material.GOLD_INGOT, 1);
+                event.getPlayer().getWorld().dropItemNaturally(block.getLocation(), bi);
+                block.setType(Material.AIR);
+                event.setCancelled(true);
+            } else if (block.getType() == Material.GLASS) {
+                ItemStack bi = new ItemStack(Material.GLASS, 1);
+                event.getPlayer().getWorld().dropItemNaturally(block.getLocation(), bi);
+            }
+        }
+    }
+    public void onBlockPhysics(BlockPhysicsEvent event) {
+        Block block = event.getBlock();
+        if (block.getFace(BlockFace.DOWN, 1).getType() == Material.LAVA
+                && next2Piston(block)) {
+            if(block.getType() == Material.COBBLESTONE) {
+                block.setType(Material.STONE);
+                block.setData((byte)15);
+            } else if (block.getType() == Material.IRON_ORE){
+                block.setType(Material.IRON_BLOCK);
+                block.setData((byte)15);
+            } else if (block.getType() == Material.GOLD_ORE){
+                block.setType(Material.GOLD_BLOCK);
+                block.setData((byte)15);
+            } else if (block.getType() == Material.SAND) {
+                block.setType(Material.GLASS);
+                block.setData((byte)15);
+            } else if (block.getType() == Material.CLAY) {
+                block.setType(Material.BRICK);
+            }
+        }
+    }
+}
---
+++ src/feildmaster/PistonSmelter/PistonSmelter.java
@@ -0,0 +1,20 @@
+package feildmaster.PistonSmelter;
+
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PistonSmelter extends JavaPlugin {
+    private final DetectPhysics blockListener = new DetectPhysics();
+
+    public void onDisable() {}
+
+    public void onEnable() {
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvent(Event.Type.BLOCK_PHYSICS, blockListener, Event.Priority.Normal, this);
+        pm.registerEvent(Event.Type.BLOCK_BREAK, blockListener, Event.Priority.Normal, this);
+
+        System.out.println("Piston Smelter v1.0: Enabled");
+    }
+    
+}
